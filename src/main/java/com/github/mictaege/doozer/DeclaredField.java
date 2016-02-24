cc749893f46d8a62e05610e98688c4d5a45f808d
mictaege <mictaege@googlemail.com>
2016-02-24 22:25:18 +0100

    #1 Fix SonarQube warnings

--- src/main/java/com/github/mictaege/doozer/DeclaredField.java
+++ src/main/java/com/github/mictaege/doozer/DeclaredField.java
@@ -4,6 +4,7 @@ package com.github.mictaege.doozer;
  * Interface for all enumerations that declares the fields of an object.
  * @param <T> The type of the object
  */
+@SuppressWarnings("squid:S2326")
 public interface DeclaredField<T> {
 
 	String name();
